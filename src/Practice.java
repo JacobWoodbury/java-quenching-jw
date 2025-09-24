42a480958083be88a3cfc553430969c2e68abd95
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-09-23 19:24:10 -0700

    finished

--- src/Practice.java
+++ src/Practice.java
@@ -257,6 +257,23 @@ public class Practice {
      * @return the count of vertices with outdegree 0
      */
     public static int sinkCount(Vertex<Integer> start) {
-        return 0;
+        
+        return sinkCount(start, 0);
+    }
+
+    public static int sinkCount(Vertex<Integer> current, int sinks){
+        if(current == null){
+          
+        }else{
+            if(current.neighbors.size() == 0){
+                sinks++;
+            }
+                
+            for(Vertex<Integer> neighbor: current.neighbors){
+                sinks = sinkCount(neighbor, sinks);
+            }
+        }
+        
+        return sinks;
     }
 }
